c9290d5a3f05acf46105a20a11c0fb9892e1e03a
void2610 <izumisan2610@gmail.com>
2025-04-29 00:08:33 +0900

    LitMotionのチュートリアル

--- Assets/Tutorial/UniTask/UniTaskTutorialUI.cs
+++ Assets/Tutorial/UniTask/UniTaskTutorialUI.cs
@@ -4,7 +4,7 @@ using R3;
 using R3.Triggers;
 using TMPro;
 
-// R3を使うには以下の名前空間をusingする
+// UniTaskを使うには以下の名前空間をusingする
 using Cysharp.Threading.Tasks;
 
 
@@ -12,24 +12,24 @@ public class UniTaskTutorialUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text text;
     
-    private string _message = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private const string DISPLAY_MESSAGE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
     // 1文字ずつ表示する
     // キャンセルされたらスキップして全部表示する
-    private async UniTask PrintAsync(string message, TMP_Text text, CancellationToken cancellationToken = default)
+    private async UniTask PrintAsync(string message, TMP_Text t, CancellationToken cancellationToken = default)
     {
-        text.text = "";
+        t.text = "";
         for (var i = 0; i < message.Length; i++)
         {
             // キャンセルされたらスキップして全部表示する
             if (cancellationToken.IsCancellationRequested)
             {
-                text.text = message;
+                t.text = message;
                 return;
             }
             
             // 1文字ずつ表示する
-            text.text += message[i];
+            t.text += message[i];
             await UniTask.Delay(100);
             // 下のように書くと、キャンセルされた時点で関数の処理が終了する
             // await UniTask.Delay(100, cancellationToken: cancellationToken);
@@ -53,7 +53,7 @@ public class UniTaskTutorialUI : MonoBehaviour
             .AddTo(this);
         
         // 非同期処理を開始する
-        await PrintAsync(_message, text, cancellationToken);
+        await PrintAsync(DISPLAY_MESSAGE, text, cancellationToken);
         // テキスト表示処理が終わったらログ
         Debug.Log("終了");
     }
