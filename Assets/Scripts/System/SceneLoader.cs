5bd9b310fd565dc4324beac7a41be5fe2525cbe7
void2610 <izumisan2610@gmail.com>
2025-04-30 14:22:22 +0900

    個人のディレクトリを作成

---
+++ Assets/Scripts/System/SceneLoader.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using R3;
+using Cysharp.Threading.Tasks;
+
+public sealed class SceneLoader : MonoBehaviour
+{
+    [Header("Bootstrap を除いて Additive でロードするシーン名")]
+    [SerializeField] private List<string> scenesToLoad = new() { "TestPlayer", "TestUI" };
+
+    public ReactiveProperty<float> Progress { get; } = new(0f);
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private async UniTaskVoid Start()
+    {
+        if (scenesToLoad.Count == 0)
+        {
+            Debug.LogError("scenesToLoad が空です");
+            return;
+        }
+
+        var activeName = scenesToLoad[0];
+
+        await LoadAdditiveScenesAsync();
+
+        var activeScene = SceneManager.GetSceneByName(activeName);
+        if (activeScene.IsValid() && activeScene.isLoaded)
+        {
+            SceneManager.SetActiveScene(activeScene);
+        }
+        else
+        {
+            Debug.LogError($"Scene '{activeName}' がロードされていません。Build Settings と名前を確認してください。");
+        }
+    }
+
+    private async UniTask LoadAdditiveScenesAsync()
+    {
+        var total = scenesToLoad.Count;
+
+        for (var i = 0; i < total; i++)
+        {
+            var sceneName = scenesToLoad[i];
+            var op = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            var tmp = i;
+            var reporter = new System.Progress<float>(p =>
+            {
+                Progress.Value = (tmp + p) / total;
+            });
+            await op.ToUniTask(progress: reporter);
+            // isLoaded==true になるまで待機
+            await UniTask.WaitUntil(() => SceneManager.GetSceneByName(sceneName).isLoaded);
+        }
+
+        Progress.Value = 1f;
+    }
+
+    public async UniTask UnloadAllScenesAsync()
+    {
+        for (var i = scenesToLoad.Count - 1; i >= 0; i--)
+        {
+            var scn = SceneManager.GetSceneByName(scenesToLoad[i]);
+            if (scn.isLoaded)
+            {
+                var op = SceneManager.UnloadSceneAsync(scn);
+                await op.ToUniTask();
+            }
+        }
+    }
+}
