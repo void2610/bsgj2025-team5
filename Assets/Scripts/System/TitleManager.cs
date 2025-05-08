86eddb52686d8c13636d1beb6c7aa4f5dc82a2c8
ItoKengo <qianwuyiteng851@gmail.com>
2025-05-08 16:51:32 +0900

    ゲーム終了ボタンの追加
    ゲーム終了ボタンを追加しました。
    プラットフォームごとに動作を変更しています。

--- Assets/Scripts/System/TitleManager.cs
+++ Assets/Scripts/System/TitleManager.cs
@@ -7,4 +7,13 @@ public class TitleManager : MonoBehaviour
     {
         SceneManager.LoadScene("MainScene");
     }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else 
+            Application.Quit();
+        #endif
+    }
 }
