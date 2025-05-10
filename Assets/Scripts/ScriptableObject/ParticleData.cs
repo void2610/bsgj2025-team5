b6d44a88c6ff58218b6b7c631297edd11c8eabd8
void2610 <izumisan2610@gmail.com>
2025-05-10 19:44:38 +0900

    ParticleManagerを実装

---
+++ Assets/Scripts/ScriptableObject/ParticleData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ParticleData", menuName = "ScriptableObject/ParticleData")]
+public class ParticleData : ScriptableObject
+{
+    public GameObject particlePrefab;
+}
--- Assets/Scripts/System/SeManager.cs
+++ Assets/Scripts/System/SeManager.cs
@@ -8,6 +8,7 @@ using UnityEngine.Audio;
 /// <summary>
 /// 効果音（SE）を一括管理するシングルトンマネージャー
 /// AudioSource のプールを使って、複数同時再生やボリューム制御に対応する
+/// プールのサイズを超えて再生できないので、パフォーマンスや音量を圧迫することがない
 /// SeDataを入力することでSEの再生を行う
 /// </summary>
 public class SeManager : SingletonMonoBehaviour<SeManager>
