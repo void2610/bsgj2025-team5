ff1a6fbad504bd8173204163fa746ba0b579c54e
void2610 <izumisan2610@gmail.com>
2025-05-17 14:53:46 +0900

    クロスフェードを実装

--- Assets/Scripts/System/BGMManager.cs
+++ Assets/Scripts/System/BGMManager.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
+using LitMotion;
 using R3;
+using UnityEngine.Serialization;
 
 /// <summary>
 /// プレイヤーの速度に応じてBGMを変更するクラス
@@ -10,18 +13,43 @@ using R3;
 public class BGMManager : MonoBehaviour
 {
     [SerializeField] private List<AudioClip> bgmClips;
+    [SerializeField] private float fadeTime = 1f; // フェード時間
+    [SerializeField] private float throttleTime = 2f; // スロットル時間
     
     private int _currentBGMIndex = 0;
     private AudioSource _audioSource;
+    private MotionHandle _fadeHandle;
     
-    private void OnChangePlayerSpeedInt(int value)
+    private async UniTask CrossFadeToAsync(int newIndex)
     {
-        if (_currentBGMIndex == value) return;
+        if (newIndex < 0 || newIndex >= bgmClips.Count) throw new ArgumentOutOfRangeException(nameof(newIndex));
+        if (_currentBGMIndex == newIndex) return;
         
-        _currentBGMIndex = value;
+        _currentBGMIndex = newIndex;
+
+        try
+        {
+            await LMotion.Create(1f, 0f, fadeTime)
+                .WithEase(Ease.OutSine)
+                .Bind(v => _audioSource.volume = v)
+                .AddTo(this)
+                .ToUniTask();
+        }
+        catch (OperationCanceledException) {}
+
         _audioSource.Stop();
         _audioSource.clip = bgmClips[_currentBGMIndex];
         _audioSource.Play();
+
+        try
+        {
+            _fadeHandle = LMotion.Create(0f, 1f, fadeTime)
+                .WithEase(Ease.InSine)
+                .Bind(v => _audioSource.volume = v)
+                .AddTo(this);
+            await _fadeHandle.ToUniTask();
+        }
+        catch (OperationCanceledException) {}
     }
 
     private void Awake()
@@ -38,7 +66,8 @@ public class BGMManager : MonoBehaviour
         // プレイヤーの速度を監視
         GameManager.Instance.Player.PlayerSpeedInt
             .DistinctUntilChanged()
-            .ThrottleFirst(TimeSpan.FromSeconds(2)) // 最低2秒間隔で切り替え
-            .Subscribe(OnChangePlayerSpeedInt).AddTo(this);
+            .ThrottleFirst(TimeSpan.FromSeconds(throttleTime)) // スロットル時間を設定
+            .Subscribe(async index => await CrossFadeToAsync(index))
+            .AddTo(this);
     }
 }
