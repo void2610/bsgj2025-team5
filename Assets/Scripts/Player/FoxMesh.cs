e36aaa31d5fb0efe8ade070dd568bf574fd483c2
void2610 <izumisan2610@gmail.com>
2025-05-19 21:01:24 +0900

    不要なイベントメソッドを削除

--- Assets/Scripts/Player/FoxMesh.cs
+++ Assets/Scripts/Player/FoxMesh.cs
@@ -4,13 +4,6 @@ public class FoxMesh : MonoBehaviour
 {
 	[SerializeField] private GameObject playerSphere;
 	
-    private Rigidbody _playerRb;
-    
-    private void Awake()
-    {
-        _playerRb = playerSphere.GetComponent<Rigidbody>();
-    }
-    
     private void Update()
     {
         this.transform.position = playerSphere.transform.position;
