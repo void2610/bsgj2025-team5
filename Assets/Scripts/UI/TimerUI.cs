7bad218e545e6bff795e1b9838d553e3dda60c77
uciyama886 <xneishan51@gmail.com>
2025-06-29 19:05:32 +0900

    fix timepush logic
    # 残り時間の変更を購読者に通知する仕組みをR3を使うように修正
    - GameManagerがReactivePropertyであるOnTimeChanged(残り時間)をR3のObservableによって通知プッシュをするように変更
    - TimerUIではGameManagerのOnTImeChangedに対して購読するように変更

--- Assets/Scripts/UI/TimerUI.cs
+++ Assets/Scripts/UI/TimerUI.cs
@@ -1,36 +1,29 @@
 using UnityEngine;
 using TMPro;
+using R3;
 
 public class TimerUI : MonoBehaviour
 {
-    // UIのTextMeshProの束縛
     [SerializeField] private TextMeshProUGUI timerText;
 
-    /// <summary>
-    /// 
-    /// </summary>
     private void Start()
     {
-        // GameManagerのインスタンスが存在し、初期化されていることを確認
         if (GameManager.Instance != null)
         {
-            // GameManagerのOnTimeChangedイベントにSetTimeDisplayメソッドを登録
-            GameManager.Instance.OnTimeChanged += SetTimeDisplay;
-
-            // 初期時間を表示させる
-            SetTimeDisplay(GameManager.Instance.CurrentTimeValue);
+            GameManager.Instance.OnTimeChanged
+                .Subscribe(SetTimeDisplay)
+                .AddTo(this);
+        }
+        else
+        {
+            Debug.LogError("GameManagerのインスタンスが見つかりません。シングルトンが正しく初期化されているか確認してください。", this);
         }
     }
 
-    /// <summary>
-    /// UIに残り時間をセットするメソッド
-    /// </summary>
     private void SetTimeDisplay(float currentTime)
     {
-        // 残り時間を分秒で計算する(かならず0以上にする)
         int minutes = Mathf.Max(0, Mathf.FloorToInt(currentTime / 60));
         int seconds = Mathf.Max(0, Mathf.FloorToInt(currentTime % 60));
-        // 表示形式をM分S秒に揃える
         timerText.text = $"{minutes:00}:{seconds:00}";
     }
 }
